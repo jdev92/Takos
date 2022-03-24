1e72880d5d0459e367f7c61f6292fd324668e933
jdev92 <juliencavaillon6@gmail.com>
2022-03-24 19:23:27 +0100

    ajout infoView.php

---
+++ view/infoView.php
@@ -0,0 +1,155 @@
+<h2>Les forfaits aéroports</h2>
+<div class="forfait">
+    <div class="roissy">
+        <h3>Roissy Charles De Gaulle</h3>
+        <img src="img/avion2.png" alt="photo d'un avion">
+        <div class="prixRoissy">
+            <p>53€ ou 58€</p>
+            <p>pour Roissy CDG</p>
+        </div>
+        <div class="conditionRoissy">
+            <p>Selon la prise en charge ou dépose Rive droite ou Gauche</p>
+        </div>
+    </div>
+
+    <div class="imgForfait">
+        <img src="img/forfait_aeroport.webp" alt="">
+    </div>
+
+    <div class="orly">
+        <h3>Orly</h3>
+        <img src="img/avion2.png" alt="photo d'un avion">
+        <div class="prixOrly">
+            <p>32€ ou 37€</p>
+            <p>pour Orly</p>
+        </div>
+        <div class="conditionOrly">
+            <p>Selon la prise en charge ou dépose Rive droite ou Gauche</p>
+
+        </div>
+    </div>
+</div><br>
+
+<div class="tarifApproche">
+    <h2>Les forfaits d'approche</h2>
+    <p>Applicables aux courses dont l’adresse de prise en charge est située dans la zone des taxis
+        parisiens.</p>
+    <ul>
+        <li>4 € pour les commandes immédiates</li>
+        <li>7 € pour les commandes à l'avance</li>
+    </ul>
+</div><br>
+
+<div class="infoTarif">
+    <h2>Les tarifs du taxi parisien</h2>
+    <p>Le prix d'une course en taxi est déterminé par le compteur horokilométrique (taximètre) qui équipe
+        obligatoirement chaque taxi.</p>
+    <p>Le principe de fonctionnement du taximètre est identique à tous les taxis mais selon qu'il s'agit des
+        taxis parisiens ou des taxis de province l'application des termes kilométriques et horaires diffère.</p>
+    <p>Le montant de la prise en charge est de 2,60 €, quelle que soit la course. <br> Tarif A : 1,09 € le km -
+        Tarif B : 1,38 € e le km - Tarif C : 1,61 € le km <br> Heure d'attente : Tarif A : 33,48 € l'heure -
+        Tarif B : 42,41 € l'heure - Tarif C : 35,99 € l'heure</p>
+    <p>Le détail des tarifs et de leurs applications sont définis par arrêté préfectoral pour chaque
+        département.</p>
+    <ul>
+        <li>JOUR/NUIT : c'est la prise en charge, soit le montant de départ du compteur lors de sa mise en
+            marche.</li>
+        <li>KM : tarif pour un kilomètre</li>
+        <li>HEURE : tarif horaire, il s'enclenche automatiquement dès que le compteur est en marche et que le
+            véhicule est à l'arrêt (embouteillage, attente du client) ou en alternance avec le tarif
+            kilométrique quand la vitesse atteint le point de conjonction des tarifs horaire et kilométrique
+            (vitesse du véhicule inférieure à : tarif horaire divisé par le tarif kilométrique appliqué), dans
+            ce cas la comptabilisation par le tarif kilométrique s'arrête.</li>
+    </ul><br>
+
+    <h2>Taxi Parisien :</h2>
+    <ul>
+        <li>KM A et Heure A :</li>
+        <li>- applicable de 10h à 17h dans Paris intra-muros boulevard périphérique inclus.</li>
+        <li>KM B et Heure B :</li>
+        <li>- applicable dans Paris intra-muros boulevard périphérique inclus de 17h à 10h</li>
+        <li>- applicable dans Paris intra-muros boulevard périphérique inclus de 7h à 24h les dimanches</li>
+        <li>- applicable dans Paris intra-muros boulevard périphérique inclus de 0h à 7h les jours fériés</li>
+        <li>- applicable de 7h à 19h tous les jours dans la zone suburbaine (la zone suburbaine étant composée
+            des communes extérieures à Paris mais rattachées aux taxis parisiens)</li>
+        <li>KM C et Heure C :</li>
+        <li>- applicable dans Paris intra-muros boulevard périphérique inclus de 0h à 7h les dimanches</li>
+        <li>- applicable dans la zone suburbaine de 19h à 7h</li>
+        <li>- applicable dans la zone suburbaine à toutes heures les dimanches et jour fériés</li>
+        <li>- applicable tous les jours à toute heure au-delà de la zone suburbaine.</li>
+        <li>Quel que soit le montant affiché au compteur la somme à payer ne peut être inférieure à 7.30 €</li>
+
+        <li> Des suppléments tarifaires sont applicables pour le transport de passager supplémentaire à partir
+            du 5ème ( 4 € ), pour les réservations et pour les bagages au dela de trois bagages par passager ou
+            pour
+            tout bagage nécessitant l'utilisation d'un équipement extérieur.</li>
+        <li>Depuis le 1er mars 2016 des forfaits sont applicables pour la desserte des aéroports d'Orly et de
+            Roissy au départ ou à l'arrivée de Paris intra-muros.</li>
+    </ul>
+</div>
+
+<h2>Tarifs et règlement du taxi parisien</h2>
+<div class="tableau">
+    <table class="tarif">
+        <thead>
+            <tr>
+                <th>TARIFS APPLICABLES</th>
+                <th>A</th>
+                <th>B</th>
+                <th>C</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>ZONE URBAINE
+                    <p>Paris, Boulevard périphérique compris</p>
+                </td>
+                <td>10h à 17h
+                    <p>Lundi au Samedi</p>
+                </td>
+                <td>17h à 10h
+                    <p>Lundi au Samedi</p>
+                    <p>7h à 24h</p>
+                    <p>Dimanches</p>
+                    <p>0h à 24h</p>
+                    <p>jours fériés</p>
+                </td>
+                <td>0h à 7h
+                    <p>dimanches</p>
+                    <p>y compris </p>
+                    <p>ceux fériés</p>
+                </td>
+            </tr>
+            <tr>
+                <td>ZONE URBAINE
+                    <p>Fin de la zone des taxis parisiens et</p>
+                    <p>desserte des aéroports d'Orly, de Roissy</p>
+                    <p>et du Parc des expositions de Villepinte</p>
+                </td>
+                <td></td>
+                <td>7h à 19h
+                    <p>Lundi au Samedi</p>
+                </td>
+                <td>19h à 7h
+                    <p>Lundi au Samedi</p>
+                    <p>0h à 24h</p>
+                    <p>dimanches et</p>
+                    <p>jours fériés</p>
+                </td>
+            </tr>
+            <tr>
+                <td>AU DELÀ DE LA
+                    <p>ZONE SUBURBAINE</p>
+                </td>
+                <td></td>
+                <td></td>
+                <td>quels que soient
+                    <p>le jour et l'heure</p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <p>Le compteur applique automatiquement le tarif horaire au lieu du tarif kilomètrique lorsque la
+        vitesse du taxi est inférieur à 30.28 km/h en tarif A, 29.56 km/h en tarif B et 22.66 km/h en tarif
+        C.</p>
+</div>
