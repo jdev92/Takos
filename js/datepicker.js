37f323002eabe5f29b27e0ba7aaad8ffdca12215
jdev92 <juliencavaillon6@gmail.com>
2022-03-24 18:48:10 +0100

    ajout datepicker.js

---
+++ js/datepicker.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $( "#datepicker" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        minDate : 0
+        // showOn: "button",
+        // buttonImage: "",
+        // buttonImageOnly: true,
+        // buttonText: "Select date"
+     });
+ });
+
+
+
