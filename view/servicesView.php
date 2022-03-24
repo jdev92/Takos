5bc601b78099e41e9f093097506863cc36430a5e
jdev92 <juliencavaillon6@gmail.com>
2022-03-24 19:21:41 +0100

    ajout servicesView.php

---
+++ view/servicesView.php
@@ -0,0 +1,85 @@
+<h2>Nos véhicules</h2>
+<div class="slider-container">
+    <div class="slide">
+        <img src="img/berline.jpg" alt="véhicule Berline">
+        <div class="title-img">Berline</div>
+    </div>
+
+
+    <div class="slide">
+        <img src="img/eco.jpg" alt="véhicule écologique">
+        <div class="title-img">Eco</div>
+    </div>
+
+    <div class="slide">
+        <img src="img/van.jpg" alt="véhicule Van">
+        <div class="title-img">Van</div>
+    </div>
+
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div><br><br><br>
+
+<div class="services">
+    <h2>Nos services gratuits</h2>
+    <div class="logo">
+        <div class="logoLeft">
+            <ul>
+                <li><img src="img/ecologique.gif" alt="logo véhicule écologique"> Véhicule écologique sur demande</li>
+                <li><img src="img/bebe.gif" alt="logo siège bébé"> Siège bébé sur demande</li>
+                <li><img src="img/usb.gif" alt="logo chargeur USB"> Chargeur USB à bord</li>
+                <li><img src="img/climatise.gif" alt="logo véhicule climatisé"> Véhicule climatisé</li>
+            </ul>
+        </div>
+
+        <div class="logoMiddle">
+            <ul>
+                <li><img src="img/heure.gif" alt="logo disponible 24/24h"> Disponible 24/24h</li>
+                <li><img src="img/jour.gif" alt="logo disponible 7/7 jours"> Disponible 7/7 jours</li>
+                <li><img src="img/cheque.gif" alt="logo chèques acceptés"> Chèques acceptés</li>
+                <li><img src="img/nonfumeur.gif" alt="logo véhicule non fumeur"> Véhicules non fumeur</li>
+            </ul>
+        </div>
+
+        <div class="logoRight">
+            <ul>
+                <li><img src="img/wifi.gif" alt="logo wifi"> wifi à bord</li>
+                <li><img src="img/handicape.gif" alt="logo véhicule avec rampe"> Véhicule aménagé sur demande</li>
+                <li> <img src="img/cb.gif" alt="logo carte bancaire"> Cartes Bancaires acceptées</li>
+                <li><i class="fab fa-cc-amex"></i> American Express acceptées</li>
+            </ul>
+        </div>
+    </div>
+
+</div>
+
+<h2>Téléchargez notre application</h2><br><br><br>
+
+<div class="application">
+    <div class="appLeft">
+        <h3>Commandez un taxi avec notre application</h3>
+        <p>Téléchargez notre application pour réserver un taxi</p>
+        <p>Disponible sur Iphone et Android</p>
+        <ol>
+            <li>Inscrivez-vous</li>
+
+            <li>Sélectionnez votre trajet, choisissez votre véhicule</li>
+
+            <li>Validez votre commande</li>
+        </ol>
+
+        <a href="https://play.google.com/store?hl=fr&gl=FR">
+            <img src="img/badge-google-play.png" alt=" image Google play">
+        </a>
+
+        <a href="https://www.apple.com/fr/app-store/">
+            <img src="img/badge-app-store.png" alt=" image Apple store">
+        </a>
+
+    </div>
+
+    <div class="appRight">
+        <img src="img/000703859_illustration_large(1).png" alt="">
+    </div>
+</div>
+<script src="js/slider.js"></script>
